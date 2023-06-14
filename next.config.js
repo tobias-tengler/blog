d8b0967e68582a992c5e1a8f401813fc3704ba82
tobias-tengler <45513122+tobias-tengler@users.noreply.github.com>
2023-06-14 21:29:16 +0200

    Use absolute basePath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: "/blog"
+    output: 'export'
+}
+
+if (process.env.NODE_ENV === 'production') {
+    nextConfig.basePath = "https://tobias-tengler.github.io/blog"
 }
 
 module.exports = nextConfig
